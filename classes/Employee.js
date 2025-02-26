b3ba2fcdf91385bde7d153683112031ec55c9740
Dustin Alandzes <dustin.alandzes@hp.com>
2025-02-26 11:29:19 -0600

    get employee static tests passing

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,12 +1,14 @@
 class Employee {
     #salary;
     #isHired;
-
+    static #allEmployees = [];
     constructor(name, position, salary) {
         this.name = name;
         this.position = position;
         this.#salary = salary;
         this.#isHired = true;
+
+        Employee.#allEmployees.push(this);
     }
 
     getSalary() {
@@ -27,6 +29,14 @@ class Employee {
         } else if (command === "fire") {
             this.#isHired = false;
         }
+    }
+
+    static getEmployees() {
+        return Employee.#allEmployees;
+    }
+
+    static getTotalPayroll() {
+        return Employee.#allEmployees.reduce((accumulator, employee) => accumulator += employee.getSalary(), 0)
     }
 }
 
